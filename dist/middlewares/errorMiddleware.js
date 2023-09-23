4b549bf597cb22983ce46d32a30d7c59511ba7e0
Hanna-Hinn <hanna.hinn30@gmail.com>
2023-09-23 18:26:31 +0300

    remove the dist file

--- dist/middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMiddleware = void 0;
-const errorMiddleware = (err, req, res) => {
-    console.error(err.message || err.stack);
-    res.status(500).json({ error: `Something Went Wrong: ${err.message}` });
-};
-exports.errorMiddleware = errorMiddleware;
-//# sourceMappingURL=errorMiddleware.js.map
